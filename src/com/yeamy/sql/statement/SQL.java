9b5ba109bc3864df1caff745962b95870aee859f
Yeamy <Yeamy0754@Yeamys-MacBook-Pro.local>
2017-06-16 12:47:06 +0800

    add: simple method

--- src/com/yeamy/sql/statement/SQL.java
+++ src/com/yeamy/sql/statement/SQL.java
@@ -1,5 +1,8 @@
 package com.yeamy.sql.statement;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class SQL {
 
 	/**
@@ -7,6 +10,25 @@ public class SQL {
 	 */
 	public static String delete(String table, Clause where) {
 		StringBuilder sql = new StringBuilder("DELETE FROM `").append(table).append('`');
+		sql.append(" WHERE ");
+		where.toSQL(sql);
+		sql.append(';');
+		return sql.toString();
+	}
+
+	public static String update(String table, Map<String, Object> map, Clause where) {
+		StringBuilder sql = new StringBuilder("UPDATE `").append(table).append("` SET ");
+		boolean f = true;
+		for (Entry<String, Object> li : map.entrySet()) {
+			if (f) {
+				f = false;
+			} else {
+				sql.append(", ");
+			}
+			sql.append('`').append(li.getKey()).append('`');
+			sql.append('=');
+			SQLString.appendValue(sql, li.getValue());
+		}
 		sql.append(" WHERE ");
 		where.toSQL(sql);
 		sql.append(';');
@@ -23,6 +45,14 @@ public class SQL {
 		sql.append(" WHERE ");
 		where.toSQL(sql);
 		sql.append(" LIMIT ").append(limit);
+		sql.append(';');
+		return sql.toString();
+	}
+
+	public static String select(String table, Clause where) {
+		StringBuilder sql = new StringBuilder("SELECT * FROM `").append(table).append('`');
+		sql.append(" WHERE ");
+		where.toSQL(sql);
 		sql.append(';');
 		return sql.toString();
 	}

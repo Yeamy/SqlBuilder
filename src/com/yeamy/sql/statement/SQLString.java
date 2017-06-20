14245217d1992b2ef7c0274b5da1c2198955f58e
Yeamy <Yeamy0754@Yeamys-MacBook-Pro.local>
2017-06-20 17:13:29 +0800

    update: add NULL support, fix code

--- src/com/yeamy/sql/statement/SQLString.java
+++ src/com/yeamy/sql/statement/SQLString.java
@@ -14,34 +14,35 @@ public interface SQLString {
 	 * 检查数据安全，添加转义符
 	 */
 	public static void appendValue(StringBuilder sb, Object value) {
-		if (value instanceof Number) {
+		if (value == null) {
+			sb.append("NULL");
+		} else if (value instanceof Number) {
 			sb.append(value);
-			return;
 		} else if (value instanceof SQLString) {
 			((SQLString) value).toSQL(sb);
-			return;
-		}
-		// as String
-		String out = value.toString();
-		sb.append('\'');
-		// replace all '
-		int l = out.length();
-		int start = 0;
-		int end;
-		while (true) {
-			end = out.indexOf('\'', start);
-			if (end == -1) {
-				sb.append(out, start, l);
-				break;
-			}
-			end += 1;
-			sb.append(out, start, end);
+		} else {
+			// as String
+			String out = value.toString();
 			sb.append('\'');
-			start = end;
-			if (start >= l) {
-				break;
+			// replace all '
+			int l = out.length();
+			int start = 0;
+			int end;
+			while (true) {
+				end = out.indexOf('\'', start);
+				if (end == -1) {
+					sb.append(out, start, l);
+					break;
+				}
+				end += 1;
+				sb.append(out, start, end);
+				sb.append('\'');
+				start = end;
+				if (start >= l) {
+					break;
+				}
 			}
+			sb.append('\'');
 		}
-		sb.append('\'');
 	}
 }

3f40937465651a2895a61415db19ff27bddbbfc2
Yeamy0754 <Yeamy0754@Yeamys-MacBook-Pro.local>
2017-11-25 14:39:39 +0800

    insert + if not exists

--- src/com/yeamy/sql/statement/Insert.java
+++ src/com/yeamy/sql/statement/Insert.java
@@ -7,6 +7,7 @@ import java.util.Map.Entry;
 public class Insert implements SQLString {
 	private final String table;
 	private final LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+	private boolean ifNotExists = false;
 
 	public Insert(String table) {
 		this.table = table;
@@ -20,6 +21,15 @@ public class Insert implements SQLString {
 	public Insert addAll(Map<String, Object> cv) {
 		map.putAll(cv);
 		return this;
+	}
+
+	public Insert ifNotExists(boolean b) {
+		ifNotExists = b;
+		return this;
+	}
+
+	public Insert ifNotExists() {
+		return ifNotExists(true);
 	}
 
 	@Override
@@ -54,7 +64,11 @@ public class Insert implements SQLString {
 			}
 			SQLString.appendValue(sql, value);
 		}
-		sql.append(");");
+		if (ifNotExists) {
+			sql.append(") IF NOT EXISTS;");
+		} else {
+			sql.append(");");
+		}
 	}
 
 	@Override

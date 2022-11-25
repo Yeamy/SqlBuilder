9a76bb940bc5b2b5446943eac921a3a030f5df89
Yeamy0754 <yeamy0754@hotmail.com>
2022-11-26 02:12:24 +0800

    Update DateFormat.java

--- src/com/yeamy/sql/statement/date/DateFormat.java
+++ src/com/yeamy/sql/statement/date/DateFormat.java
@@ -2,12 +2,14 @@ package com.yeamy.sql.statement.date;
 
 import java.util.Date;
 
+import com.yeamy.sql.statement.AbsColumn;
 import com.yeamy.sql.statement.Column;
 import com.yeamy.sql.statement.SQLString;
 import com.yeamy.sql.statement.Searchable;
 
 public class DateFormat extends Column {
 	public String format;
+	public AbsColumn column;
 
 	/**
 	 * @param format <link>https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html#function_date-format</link>
@@ -27,10 +29,20 @@ public class DateFormat extends Column {
 		this.format = format;
 	}
 
+	public DateFormat(AbsColumn column, String format) {
+		super((String) null);
+		this.column = column;
+		this.format = format;
+	}
+
 	@Override
 	public void toSQL(StringBuilder sb) {
 		sb.append("DATE_FORMAT(");
-		super.toSQL(sb);
+		if (column != null) {
+			column.toSQL(sb);
+		} else {
+			super.toSQL(sb);
+		}
 		sb.append(", ");
 		SQLString.appendValue(sb, format);
 		sb.append(')');
